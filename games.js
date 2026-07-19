9c843bf72fc673c93b5ff6a54a7bfba5c60d9a8c
SltLele <15527781950@163.com>
2026-07-19 18:46:01 +0800

    Initial commit

---
+++ games.js
@@ -0,0 +1,20 @@
+// 游戏列表配置
+var gamesData = [
+    { name: "贪吃蛇", file: "贪吃蛇.html" },
+    { name: "俄罗斯方块", file: "俄罗斯方块.html" },
+    { name: "打砖块", file: "打砖块.html" },
+    { name: "扫雷", file: "minesweeper.html" },
+    { name: "2048", file: "2048.html" },
+    { name: "记忆翻牌", file: "记忆翻牌.html" },
+    { name: "数字拼图[滑动拼图]", file: "数字拼图[滑动拼图].html" },
+    { name: "乒乓球", file: "乒乓球.html" },
+    { name: "打靶", file: "打靶.html" },
+    { name: "Minecraft-Web 1.5.2", file: "Eaglercraft_IR_1.5.2.html", icon: "photo/m1.png" },
+    { name: "Minecraft-Web 1.8.8", file: "Eaglercraft_IR_1.8.8.html", icon: "photo/m1.png" },
+    { name: "Minecraft-Web 1.8.8高性能", file: "Eaglercraft_IR_1.8.8wasm.html", icon: "photo/m1.webp" },
+    { name: "Minecraft-Web c0.30高性能", file: "Eaglercraft_IR_c0.30wasm.html", icon: "photo/m1.webp" },
+    { name: "[测试中]Minecraft-Web 1.12.2高性能", file: "Eaglercraft_IR_1.12.2wasm.html", icon: "photo/m1.webp" },
+    { name: "[测试中]Minecraft-Web 1.12.2", file: "Eaglercraft_IR_1.12.2.html", icon: "photo/m1.png" },
+    // 如果您的图标不是 png，可以手动指定 icon 路径，例如：
+    // { name: "我的游戏", file: "mygame.html", icon: "photo/mygame.jpg" }
+];
